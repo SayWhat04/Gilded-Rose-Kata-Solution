9b9696002cf0fdc67970405c760180d05123c086
Artur Bogusiak <artur.bogusiak@gmail.com>
2022-04-10 23:10:41 +0200

    Started extracting logic for all items to separate if blocks.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,74 +14,108 @@ class GildedRose {
     public void updateQuality() {
         // Iterate over all items
         for (Item item : items) {
-            // If item is not Backstage pass or Brie
+
+            // Sulfuras can't be altered
+            if (item.name.equals(SULFURAS)) {
+                return;
+            }
+
+            if (item.name.equals(BACKSTAGE_PASSES)) {
+                if (isQualityBelowMaxValue(item)) {
+                    item.quality = item.quality + 1;
+                    // ... and there are 10 or less days to concert...
+                    if (item.sellIn < 11) {
+
+                        // cant't breach 50 quality
+                        if (isQualityBelowMaxValue(item)) {
+
+                            // ... increase quality by total 2
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+
+                // ... and there are 5 or less days to concert...
+                if (item.sellIn < 6) {
+
+                    // cant't breach 50 quality
+                    if (isQualityBelowMaxValue(item)) {
+
+                        // ... increase quality by total 3
+                        item.quality = item.quality + 1;
+                    }
+                }
+
+            }
+
+            if (item.name.equals(AGED_BRIE)) {
+                if (isQualityBelowMaxValue(item)) {
+                    item.quality = item.quality + 1;
+                }
+            }
+
+
+            // If item is not Backstage pass or Brie, NORMAL ITEM
+            // can be moved to else block
             if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
 
                 // Item quality can't be negative
                 if (isQualityBiggerThatLowestValue(item)) {
 
                     // If item is not Sulfuras
-                    if (!item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1; // Reduce item quality. It is normal item approach
-                    }
+                    item.quality = item.quality - 1; // Reduce item quality. It is normal item approach
                 }
             } else {
-                // Item quality can't be greater that 50
-                if (isQualityBelowMaxValue(item)) {
-                    item.quality = item.quality + 1;
-
-                    // If item is Backstage Pass...
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
-                        // ... and there are 10 or less days to concert...
-                        if (item.sellIn < 11) {
-
-                            // cant't breach 50 quality
-                            if (isQualityBelowMaxValue(item)) {
-
-                                // ... increase quality by total 2
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        // ... and there are 5 or less days to concert...
-                        if (item.sellIn < 6) {
-
-                            // cant't breach 50 quality
-                            if (isQualityBelowMaxValue(item)) {
-
-                                // ... increase quality by total 3
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
+                //// Item quality can't be greater that 50
+                //if (isQualityBelowMaxValue(item)) {
+                //    item.quality = item.quality + 1;
+//
+                //    // If item is Backstage Pass...
+                //    if (item.name.equals(BACKSTAGE_PASSES)) {
+                //        // ... and there are 10 or less days to concert...
+                //        if (item.sellIn < 11) {
+//
+                //            // cant't breach 50 quality
+                //            if (isQualityBelowMaxValue(item)) {
+//
+                //                // ... increase quality by total 2
+                //                item.quality = item.quality + 1;
+                //            }
+                //        }
+//
+                //        // ... and there are 5 or less days to concert...
+                //        if (item.sellIn < 6) {
+//
+                //            // cant't breach 50 quality
+                //            if (isQualityBelowMaxValue(item)) {
+//
+                //                // ... increase quality by total 3
+                //                item.quality = item.quality + 1;
+                //            }
+                //        }
+                //    }
+                //}
             }
 
             // Only Sulfuras isn't needed to be sold
-            if (!item.name.equals(SULFURAS)) {
-                item.sellIn = item.sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
 
             // Once selling date has passed, quality will drop twice as fast
             if (item.sellIn < 0) {
 
                 // Quality for Brie will increase as days passed
-                if (!item.name.equals(AGED_BRIE)) {
+                if (item.name.equals(AGED_BRIE)) {
 
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (isQualityBiggerThatLowestValue(item)) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1; // Reduce item quality. It is normal item approach
-                            }
-                        }
-                    } else {
-                        // If concert date passed, quality of Backstage Pass will drop to 0
-                        item.quality = 0;
-                    }
-                } else {
                     // This condition will be met for Brie only
                     if (isQualityBelowMaxValue(item)) {
                         item.quality = item.quality + 1;
+                    }
+                } else if (item.name.equals(BACKSTAGE_PASSES)) {
+                    // If concert date passed, quality of Backstage Pass will drop to 0
+                    item.quality = 0;
+                } else {
+                    if (isQualityBiggerThatLowestValue(item)) {
+                        item.quality = item.quality - 1; // Reduce item quality. It is normal item approach
                     }
                 }
             }
