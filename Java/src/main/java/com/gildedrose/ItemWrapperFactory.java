32ba1e9b47e80e7a521b2d21499e00a85f643244
Artur Bogusiak <artur.bogusiak@gmail.com>
2022-04-14 22:37:22 +0200

    Introduced Conjured item type.

--- Java/src/main/java/com/gildedrose/ItemWrapperFactory.java
+++ Java/src/main/java/com/gildedrose/ItemWrapperFactory.java
@@ -3,9 +3,11 @@ package com.gildedrose;
 import com.gildedrose.item.*;
 
 public class ItemWrapperFactory {
+    // TODO: Extract to enum
     private static final String AGED_BRIE = "Aged Brie";
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String CONJURED = "Conjured";
 
     public static ItemWrapper create(Item item) {
         if (item.name.equals(SULFURAS)) {
@@ -14,6 +16,8 @@ public class ItemWrapperFactory {
             return new BackstagePasses(item);
         } else if (item.name.equals(AGED_BRIE)) {
             return new AgedBrie(item);
+        } else if (item.name.contains(CONJURED)) {
+            return new ConjuredItem(item);
         } else {
             return new RegularItem(item);
         }
---
+++ Java/src/main/java/com/gildedrose/item/ConjuredItem.java
@@ -0,0 +1,34 @@
+package com.gildedrose.item;
+
+import com.gildedrose.Item;
+
+public class ConjuredItem implements ItemWrapper {
+    private static final int MIN_QUALITY = 0;
+    private static final int MAX_QUALITY = 50;
+    private Item item;
+
+    public ConjuredItem(Item item) {
+        this.item = item;
+    }
+
+    @Override
+    public void updateItemState() {
+        updateSellIn();
+        updateQuality();
+    }
+
+    private void updateSellIn() {
+        item.sellIn = item.sellIn - 2;
+    }
+
+    private void updateQuality() {
+        item.quality = item.quality - 1;
+        
+        if (item.sellIn < 0) {
+            item.quality = item.quality - 1;
+        }
+        if (item.quality < MIN_QUALITY) {
+            item.quality = MIN_QUALITY;
+        }
+    }
+}
